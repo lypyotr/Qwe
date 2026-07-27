df9c39e717c21fd0759278f76b0f5cc1493a6aa1
Dolf <lypyotr@yandex.ru>
2026-07-27 18:10:29 +0300

    redesign messenger interface

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='salary-v45';
-const STATIC=['./index.html','./manifest.json','./icon.svg','./messenger-v2.js?v=5'];
+const CACHE='salary-v46';
+const STATIC=['./index.html','./manifest.json','./icon.svg','./messenger-v2.js?v=6'];
 
 // Dynamic APIs that must NEVER be touched by the service worker: Supabase
 // (REST/auth/realtime) and the exchange-rate sources. Caching these cache-first
